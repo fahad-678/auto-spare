3be6427d5e728924b5ea02c6dbbd2042032d8cfd
Fahad <fahadharoon678@gmail.com>
2024-07-23 14:01:31 +0500

    feat(debug-bar): added laravel debug bar

--- config/app.php
+++ config/app.php
@@ -195,7 +195,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -211,6 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ])->toArray(),
 
 ];
