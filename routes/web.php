ea5233a795117cd1c2ce62d45440b638ab5b2bf8
Fahad <fahadharoon678@gmail.com>
2025-01-28 13:56:49 +0500

    fix(routes): update category resource routing to use CategoryController

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::middleware(['auth'])->group(function () {
     Route::resource('/products', ProductController::class)->except(['index', 'show']);
-    Route::resource('/category', ProductController::class)->except(['index', 'show']);
+    Route::resource('/category', CategoryController::class)->except(['index', 'show']);
 });
 
 Route::middleware(['web'])->group(function () {
@@ -47,8 +47,8 @@ Route::middleware(['web'])->group(function () {
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
     Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
 
+    Route::resource('/category', CategoryController::class)->except(['create', 'store', 'edit', 'update', 'destroy']);
     Route::get('/category/search', [CategoryController::class, 'search'])->name('category.search');
-    Route::resource('/category', CategoryController::class)->only(['index', 'show']);
 
     Route::get('/about-us', [DashboardController::class, 'aboutUs'])->name('about-us');
     Route::get('/contact-us', [DashboardController::class, 'contactUs'])->name('contact-us');
