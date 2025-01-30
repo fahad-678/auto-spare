4d9958e118ed48d701f0cf4d54173b761f6fefd7
Fahad <fahadharoon678@gmail.com>
2025-01-30 16:42:36 +0500

    fix(newsletter): add target and rel attributes to social media links for improved security and usability

--- resources/views/pages/dashboard/newsletter.blade.php
+++ resources/views/pages/dashboard/newsletter.blade.php
@@ -2,11 +2,13 @@
     <div class="row align-items-center container">
         <div class="col-md-3 ms-md-12">
             <div class="d-flex justify-content-center justify-content-md-start">
-                <a href="https://www.facebook.com/share/12CwmRoAFAV/?mibextid=wwXIfr" class="me-3"><i
+                <a href="https://www.facebook.com/share/12CwmRoAFAV/?mibextid=wwXIfr" target="_blank"
+                    rel="noopener noreferrer" class="me-3"><i
                         class="fab fa-facebook-f text-primary border py-2 px-4 border-gray-600"></i></a>
                 <a href="#" class="me-3"><i
                         class="fab fa-twitter text-primary border py-2 px-4 border-gray-600"></i></a>
-                <a href="https://www.instagram.com/nkautopartsdxb?igsh=Y290cDA1Y28zM2J1&utm_source=qr" class="me-3"><i
+                <a href="https://www.instagram.com/nkautopartsdxb?igsh=Y290cDA1Y28zM2J1&utm_source=qr" target="_blank"
+                    rel="noopener noreferrer" class="me-3"><i
                         class="fab fa-instagram text-primary border py-2 px-4 border-gray-600"></i></a>
                 <a href="#" class="me-3"><i
                         class="fab fa-behance text-primary border py-2 px-4 border-gray-600"></i></a>
